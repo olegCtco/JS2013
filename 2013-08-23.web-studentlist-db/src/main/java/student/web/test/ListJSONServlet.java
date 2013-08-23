15c34341e9c285618cd17a9e1807925567494dbc
denis <denis.udod@neueda.com>
2013-08-23 21:44:24 +0300

    add JSON

---
+++ 2013-08-23.web-studentlist-db/src/main/java/student/web/test/ListJSONServlet.java
@@ -0,0 +1,26 @@
+package student.web.test;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import student.app.test.ListTest;
+import student.domain.Student;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(urlPatterns = {"/test/json"})
+public class ListJSONServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ListTest app = new ListTest();
+        List<Student> students = app.getStudents();
+        req.setAttribute("students", students);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(resp.getWriter(), students);
+    }
+}
+
